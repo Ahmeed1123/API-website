fef6af92d9111aac278cba133ab862a144c67891
Ahmed__11 <ahmedsho636@gmail.com>
2023-09-12 22:27:06 +0300

    Revert "Initial commit"
    This reverts commit 75191f6c175c290b9d37f9778f4ff2771069976b.

--- final-test/asstis/js/indexLogic.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", () => {
-    const endOfPage = window.innerHeight + window.pageYOffset >= document.body.scrollHeight;
-    if (endOfPage && currentPage < 217 ) {
-
-        currentPage = currentPage + 1 ;
-        readReqost(false , currentPage);
-    }
-});
